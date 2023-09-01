c366b55e6a5e88a366edf4d9ca4b7ece7295736a
mitahutagalung966 <mitahutagalung09@gmail.com>
2023-09-01 11:25:42 +0700

    commit message

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-type = "text/javascript";
-src = "https://cdn.prinsh.com/NathanPrinsley-effect/efek-salju.js";
 
-src =
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js";
-integrity =
-  "sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm";
-crossorigin = "anonymous";
-const tambah = document.getElementById("list");
-function cetak() {
-  let isi = document.getElementById("tambh").value;
 
-  tambah.innerHTML = tambah.innerHTML + `<li>${isi}</li>`;
-}
+
